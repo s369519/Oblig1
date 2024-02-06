88f08cbce903975855aebdd2800eb1a58aafd54b
s369519 <s369519@oslomet.no>
2024-02-06 14:21:56 +0100

    12

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,18 @@
+const biletter=[];
+
+
+
+
 function kjop(){
+    const film = document.getElementById("film").value;
+    const antallS = document.getElementById("antall").value;
+    const antall = Number(antallS);
+    const fornavn = document.getElementById("fornavn").value;
+    const etternavn = document.getElementById("etternavn").value;
+    const telefonnrS = document.getElementById("telefonnr").value;
+    const telefonnr = Number(telefonnrS);
+    const epost = document.getElementById("epost").value;
+
 
 }
 
