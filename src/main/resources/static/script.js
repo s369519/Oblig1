172f4a401eaf306c1b5a8760c8cb4208baf0e9e0
s369519 <s369519@oslomet.no>
2024-02-06 14:41:14 +0100

    adsasd

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,4 @@
-const biletter=[];
-
-
+const billetter=[];
 
 
 function kjop(){
@@ -12,6 +10,28 @@ function kjop(){
     const telefonnrS = document.getElementById("telefonnr").value;
     const telefonnr = Number(telefonnrS);
     const epost = document.getElementById("epost").value;
+
+    if (isNaN(antall)){
+        alert(" Antallet er ikke et tall!");
+        error=true;
+    }else if(antall<1){
+        alert("Antallet må være større enn 0 !")
+        error=true;
+    }else{
+        error = false;
+    }
+
+
+    if (isNaN(telefonnr)){
+        alert(" Antallet er ikke et tall!");
+        error=true;
+    }else if(telefonnr<1){
+        alert("Antallet må være mer enn 0 !")
+        error=true;
+    }else{
+        error = false;
+    }
+
 
 
 }
@@ -29,6 +49,7 @@ function sjekk(){
 
 
 function slett(){
-
+    billetter.length = 0;
+    document.getElementById("personRegister").innerHTML="";
 }
 
