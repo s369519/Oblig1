b96189f9d858282adb822ee4044d81b032894e5c
s369519 <s369519@oslomet.no>
2024-02-07 18:10:10 +0100

    fix

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -20,7 +20,12 @@ function kjop(){
     let feilEpost=document.getElementById("feilEpost");
 
     var epostRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //epostRegex sikrer at det er standard format på epost-adressen, altså x@x.x
+    //https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
     var tlfRegex = /^(0047|\+47|47)?[2-9]\d{7}$/;
+    //tlfRegex sikrer at telefonnummeret oppgitt er standard norsk format, samt kan skrives med og uten +47/0047
+    //https://stackoverflow.com/questions/34001939/regular-expression-for-norwegian-numbers
+
     const filmer=document.getElementById("alleBilletter");
 
 
@@ -35,38 +40,46 @@ function kjop(){
     }
 
 
-    if (isNaN(telefonnr)){
+    if (isNaN(telefonnr.value)){
         feilTelefonnr.innerHTML = "Ugyldig telefonnummer!";
     }
     else if (telefonnr.value === ""){
         feilTelefonnr.innerHTML = "Telefonnummer må fylles ut!";
     }
-    //else if (telefonnr.length !== 8 && (telefonnr.startsWith('4') || telefonnr.startsWith('9'))){
-
-
-    //Sikrer at tlfnr er 8 siffer og starter på enten 4 eller 9 (norsk standard)
-    else if (tlfRegex.test(telefonnr) === false){
+    else if (tlfRegex.test(telefonnr.value) === false){
         feilTelefonnr.innerHTML = "Ugyldig telefonnummer";
     }
+    //else if (telefonnr.startsWith('4') || telefonnr.startsWith('9'))){}
+    //Fikk ikke til .startsWith funksjonen...
 
 
     if (film.value === "Velg Film") {
         feilFilm.innerHTML = "Du må velge en film!";
     }
 
-    if (fornavn === ""){
+
+    if (fornavn.value === ""){
         feilFornavn.innerHTML = "Fornavn må fylles ut!";
     }
+    else if (isNaN(fornavn.value) === false){
+        feilFornavn.innerHTML = "Fornavn må være bokstaver"
+    }
 
-    if (etternavn === ""){
+
+    if (etternavn.value === ""){
         feilEtternavn.innerHTML = "Etternavn må fylles ut!";
     }
+    else if (isNaN(etternavn.value) === false){
+        feilEtternavn.innerHTML = "Etternavn må være bokstaver"
+    }
 
-    if (epost === ""){
+
+
+    if (epost.value === ""){
         feilEpost.innerHTML = "Epost må fylles ut!";
     }
-    //Tester om epostadressen har grunnleggende formatering
-    else if (epostRegex.test(epost) === false){
+
+    else if (epostRegex.test(epost.value) === false){
         feilEpost.innerHTML = "Ugyldig epostadresse!";
     }
 
@@ -74,7 +87,7 @@ function kjop(){
     else {
         billetter.push({
             Film: film.value,
-            Antall: antall.value,
+            Antall: antallS.value,
             Fornavn: fornavn.value,
             Etternavn: etternavn.value,
             Telefonnr: telefonnr.value,
@@ -82,26 +95,20 @@ function kjop(){
         });
 
         filmer.innerHTML = "<tr><th>Film</th>" + "<th>Antall</th>" + "<th>Fornavn</th>" + "<th>Etternavn</th>"
-            + "<th>Telefonnr</th>" + "<th>Epost</th></tr>";
+                         + "<th>Telefonnr</th>" + "<th>Epost</th></tr>";
 
         for (let i = 0; i < billetter.length; i++) {
             filmer.innerHTML += "<tr><td>" + billetter[i].Film + "</td><td>" + billetter[i].Antall + "</td><td>"
                 + billetter[i].Fornavn + "</td><td>" + billetter[i].Etternavn + "</td><td>" + billetter[i].Telefonnr
                 + "</td><td>" + billetter[i].Epost + "</td></tr>";
-            document.write(billetter);
         }
+
     }
-
-
-
-
 
 }//Ende av kjøp()
 
-
-
 function slett(){
     billetter.length = 0;
-    document.getElementById("personRegister").innerHTML="";
+    document.getElementById("alleBilletteretter").innerHTML="";
 }
 
