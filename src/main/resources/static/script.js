8485771a5e9196fd8aff229df5a5d4dc569b330e
s369519 <s369519@oslomet.no>
2024-02-07 22:47:23 +0100

    yo

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -9,7 +9,7 @@ function kjop(){
     const etternavn = document.getElementById("etternavn");
     const telefonnrS = document.getElementById("telefonnr");
     const telefonnr = Number(telefonnrS.value);
-    const epost = document.getElementById("epost");
+    let epost = document.getElementById("epost");
 
 
     let feilFilm=document.getElementById("feilFilm");
@@ -19,9 +19,11 @@ function kjop(){
     let feilTelefonnr=document.getElementById("feilTelefonnr");
     let feilEpost=document.getElementById("feilEpost");
 
+
     var epostRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     //epostRegex sikrer at det er standard format på epost-adressen, altså x@x.x
     //https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+
     var tlfRegex = /^(0047|\+47|47)?[2-9]\d{7}$/;
     //tlfRegex sikrer at telefonnummeret oppgitt er standard norsk format, samt kan skrives med og uten +47/0047
     //https://stackoverflow.com/questions/34001939/regular-expression-for-norwegian-numbers
@@ -32,35 +34,41 @@ function kjop(){
     if (isNaN(antall)){
         feilAntall.innerHTML = " Antallet er ikke et tall!";
     }
+
     else if(antall<1) {
         feilAntall.innerHTML = "Antallet må være større enn 0!";
     }
+
     else if(antall.value === ""){
         feilAntall.innerHTML = "Antall må fylles inn!";
     }
 
 
-    if (isNaN(telefonnr.value)){
+    if (isNaN(telefonnr)) {
         feilTelefonnr.innerHTML = "Ugyldig telefonnummer!";
     }
-    else if (telefonnr.value === ""){
+
+    else if (telefonnrS.value === ""){
         feilTelefonnr.innerHTML = "Telefonnummer må fylles ut!";
     }
+
     else if (tlfRegex.test(telefonnr.value) === false){
-        feilTelefonnr.innerHTML = "Ugyldig telefonnummer";
+        feilTelefonnr.innerHTML = "Telefonnummeret må være norsk!";
     }
-    //else if (telefonnr.startsWith('4') || telefonnr.startsWith('9'))){}
-    //Fikk ikke til .startsWith funksjonen...
 
 
     if (film.value === "Velg Film") {
         feilFilm.innerHTML = "Du må velge en film!";
+    }
+    else if (film.value === ""){
+        feilFilm.innerHTML = "Du må velge en film!"
     }
 
 
     if (fornavn.value === ""){
         feilFornavn.innerHTML = "Fornavn må fylles ut!";
     }
+
     else if (isNaN(fornavn.value) === false){
         feilFornavn.innerHTML = "Fornavn må være bokstaver"
     }
@@ -69,10 +77,10 @@ function kjop(){
     if (etternavn.value === ""){
         feilEtternavn.innerHTML = "Etternavn må fylles ut!";
     }
+
     else if (isNaN(etternavn.value) === false){
         feilEtternavn.innerHTML = "Etternavn må være bokstaver"
     }
-
 
 
     if (epost.value === ""){
@@ -90,7 +98,7 @@ function kjop(){
             Antall: antallS.value,
             Fornavn: fornavn.value,
             Etternavn: etternavn.value,
-            Telefonnr: telefonnr.value,
+            Telefonnr: telefonnrS.value,
             Epost: epost.value
         });
 
@@ -102,13 +110,47 @@ function kjop(){
                 + billetter[i].Fornavn + "</td><td>" + billetter[i].Etternavn + "</td><td>" + billetter[i].Telefonnr
                 + "</td><td>" + billetter[i].Epost + "</td></tr>";
         }
-
     }
+
+
+    if (antallS.value !== "") {
+        feilAntall.innerHTML = "";
+    }
+
+    if (fornavn.value !== "") {
+        feilFornavn.innerHTML = "";
+    }
+
+    if (etternavn.value !== "") {
+        feilEtternavn.innerHTML = "";
+    }
+
+    if (telefonnrS.value !== "" && telefonnrS.value.length == 8) {
+        feilTelefonnr.innerHTML = "";
+    }
+
+    if (epost.value !== "") {
+        feilEpost.innerHTML = "";
+    }
+
+
+    film.value = "Velg film her";
+    antallS.value = "";
+    fornavn.value = "";
+    etternavn.value = "";
+    telefonnrS.value = "";
+    epost.value = "";
 
 }//Ende av kjøp()
 
 function slett(){
     billetter.length = 0;
-    document.getElementById("alleBilletteretter").innerHTML="";
+    document.getElementById("alleBilletter").innerHTML = "";
+    document.getElementById("film").value = "Velg Film";
+    document.getElementById("antall").value = "";
+    document.getElementById("fornavn").value = "";
+    document.getElementById("etternavn").value = "";
+    document.getElementById("telefonnr").value = "";
+    document.getElementById("epost").value = "";
 }
 
